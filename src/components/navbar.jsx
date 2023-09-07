08ccb40017819f5873c99335c3b167dfc58d3b2e
Karthikpeddy <peddykarthik1027@gmail.com>
2023-09-07 22:22:31 +0530

    inti_0.0.3

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,12 +6,29 @@ const Navbar = () => {
   return (
     <div>
       <div className="">
-        <img src={Logo} alt="" />
+        <img src={Logo} alt="" className="w-15 h-15 object-cover" />
       </div>
       <div>
-        <ul>
+        <ul className="list-none">
           <li>
-            <Link to="home" smooth={true} duration={500}></Link>
+            <Link to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="home" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="home" smooth={true} duration={500}>
+              Restaurants
+            </Link>
+          </li>
+          <li>
+            <Link to="home" smooth={true} duration={500}>
+              Contact
+            </Link>
           </li>
         </ul>
       </div>
